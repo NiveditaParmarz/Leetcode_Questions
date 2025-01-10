6eccfa00c0e16f7f99423f2e35df5a533c630439
Nivedita Parmar <123835781+NiveditaParmarz@users.noreply.github.com>
2025-01-10 11:26:20 +0530

    Time: 11 ms, Memory: 45.3 MB - LeetHub

--- 1834-536-914-x-of-a-kind-in-a-deck-of-cards/1834-536-914-x-of-a-kind-in-a-deck-of-cards.java
+++ 1834-536-914-x-of-a-kind-in-a-deck-of-cards/1834-536-914-x-of-a-kind-in-a-deck-of-cards.java
@@ -1,27 +1,36 @@
 class Solution {
     public boolean hasGroupsSizeX(int[] deck) {
         HashMap<Integer,Integer> hm = new HashMap<>();
-        for(int i = 0; i < deck.length; i++){
+        for(int i = 0; i < deck.length; i++){ //taking out frequencies
             hm.put(deck[i],hm.getOrDefault(deck[i],0)+1);
         }
-        int arr[] = new int[deck.length];
-        for(int i = 0; i < deck.length; i++){
-            arr[i] = hm.get(deck[i]);
-        }
+        // int arr[] = new int[deck.length]; //making a new array to store only the frequencies
+        // for(int i = 0; i < deck.length; i++){
+        //     arr[i] = hm.get(deck[i]);
+        // }
 
-        int num = gcdArr(arr);
-        if(num>1)return true;
+        // int num = gcdArr(arr); //taking the gcd of the frequencies cuz that will be the group size.
+        // if(num > 1)return true; //gcd should be greater than 1 so we can make groups
+        // else return false; //1 group size is not allowed that's why false.
+
+
+
+        //-------------------------------
+        int ans = 0;
+        for(int key:hm.keySet()){
+            ans = gcd(ans,hm.get(key));
+        }
+        if(ans>1)return true;
         else return false;
-
     }
 
-    public int gcdArr(int arr[]){
-        int ans = arr[0];
-        for(int i = 1; i < arr.length; i++){
-            ans = gcd(ans,arr[i]);
-        }
-        return ans;
-    }
+    // public int gcdArr(int arr[]){
+    //     int ans = arr[0];
+    //     for(int i = 1; i < arr.length; i++){
+    //         ans = gcd(ans,arr[i]);
+    //     }
+    //     return ans;
+    // }
 
     public int gcd(int a, int b){
         if(a==0)return b;
